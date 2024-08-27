1ee34db4ce15ead34bdb2a259e4176c2950dc82b
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-08-27 22:19:33 +0530

    chore: setup shadcn

--- packages/search-app/vite.config.ts
+++ packages/search-app/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'SearchModal',
+      fileName: (format) => `search-modal.${format}.js`
+    }
+  },
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib')
+    }
+  }
 })
