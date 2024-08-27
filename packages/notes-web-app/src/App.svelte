5551d784d487b167c5891f2e6c2b63c38b4d57d0
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-08-28 01:41:28 +0530

    :tada: notes web-app

---
+++ packages/notes-web-app/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import AddNote from "$lib/AddNote.svelte";
+  import "./app.css";
+</script>
+
+<main class="flex flex-col items-center w-full h-full p-24">
+  <AddNote />
+</main>
---
+++ packages/notes-web-app/src/lib/AddNote.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  let input: HTMLInputElement;
+  let notes: string[] = [];
+
+  function addNote() {
+    notes.push(input.value);
+    input.value = "";
+    notes = notes;
+  }
+
+  function removeNote(index: number) {
+    notes.splice(index, 1);
+    notes = notes;
+  }
+</script>
+
+
+<form on:submit|preventDefault={addNote}>
+  <input
+  bind:this={input}
+  class="focus-within:outline-none"
+  placeholder="Start typing..."
+  />
+</form>
+
+<div class="grid grid-flow-row grid-cols-3 gap-7">
+  {#each notes as note, i}
+    <div>
+      <p>#{i} {note}</p>
+      <button on:click={() => removeNote(i)}>[x]</button>
+    </div>
+  {/each}
+</div>
