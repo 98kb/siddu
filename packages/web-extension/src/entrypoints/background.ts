375286b3be7801589dd40d0b5296ed7faa9d712e
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-01 00:41:08 +0530

    feat(background): use request router to handle messages

--- packages/web-extension/src/entrypoints/background.ts
+++ packages/web-extension/src/entrypoints/background.ts
@@ -1,33 +1,12 @@
-import {Fact, db} from "@repo/facts-db";
+import {FactsORM} from "@repo/facts-db";
+import {RuntimeAdapter, createRouter} from "@repo/runtime-messaging";
 
-type Message =
-  | {
-      type: "search";
-      query: string;
-    }
-  | {
-      type: "getAll";
-    };
-type Handler = (message: Message, sendResponse: (r: any) => any) => void;
-
-const handlers: Record<Message["type"], Handler> = {
-  getAll: (_, sendResponse) => {
-    db.notes.toArray().then(notes => sendResponse(notes));
-  },
-  search: (message, sendResponse) => {
-    const filterNotes = (note: Fact) => note.content.includes(message.query);
-    db.notes
-      .filter(note => filterNotes(note))
-      .toArray()
-      .then(notes => sendResponse(notes));
-  },
-};
+const db = new FactsORM(new RuntimeAdapter());
+const reply = createRouter(db);
 
 export default defineBackground(() => {
-  chrome.runtime.onMessage.addListener(
-    (message: Message, sender, sendResponse) => {
-      handlers[message.type]?.(message, sendResponse);
-      return true;
-    },
-  );
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    reply(message, sendResponse);
+    return true;
+  });
 });
