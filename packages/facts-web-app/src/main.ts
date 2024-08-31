6ff7ba4c63149381bc522ad6e5122189d8566297
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-01 00:15:41 +0530

    refactor: replace FactDB with FactORM

--- packages/facts-web-app/src/main.ts
+++ packages/facts-web-app/src/main.ts
@@ -1,11 +1,11 @@
-import { FactsDb, LocalAdapter } from '@repo/facts-db';
+import {FactsORM, LocalAdapter} from '@repo/facts-db';
 import './app.css'
 import App from './App.svelte'
 
 const app = new App({
   target: document.getElementById('app')!,
   props: {
-    db: new FactsDb(new LocalAdapter("notes")),
+    db: new FactsORM(new LocalAdapter("facts")),
   }
 })
 
