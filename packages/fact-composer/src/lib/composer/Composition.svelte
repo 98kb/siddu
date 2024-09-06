ad45e61d421abd7ff3f829e4a594802c3ddb9009
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-06 19:36:19 +0530

    feat(composer): Add composition layout

--- packages/fact-composer/src/lib/composer/Composition.svelte
+++ packages/fact-composer/src/lib/composer/Composition.svelte
@@ -1,36 +1,38 @@
 <script lang="ts">
   import * as Command from "$lib/components/ui/command";
-  import type {FactsORM, Fact} from "@repo/facts-db";
-  import CompositionEditor from "./CompositionEditor.svelte";
-  import Button from "$lib/components/ui/button/button.svelte";
+  import type {ORM, Fact} from "@repo/facts-db";
+  import Button from "$ui/button/button.svelte";
+  import { concat } from "$lib/concat";
+  import CompositionLayout from "./CompositionLayout.svelte";
+  import UnstyledTextarea from "$lib/components/ui/textarea/UnstyledTextarea.svelte";
 
   export let placeholder = "";
-  export let db: FactsORM;
+  export let facts: ORM<"facts">;
   let query = "";
   let value = "";
 
-  const facts = db.toObservable(() => db.objects.getAll());
+  const facts$ = facts.toObservable(() => facts.objects.getAll());
 
-  const concatValue = (newValue: string) => {
-    value = [value, newValue].filter(Boolean).join("\n");
+  const appendValue = (str: string) => {
+    value = concat("\n")(value, str);
   };
 
   const handleSelect = (fact: Fact) => {
-    concatValue(fact.content);
+    appendValue(fact.content);
     query = "";
   };
 
   const addFact = async () => {
     if (query) {
-      await db.objects.addOne({content: query});
-      concatValue(query);
+      await facts.objects.addOne({content: query});
+      appendValue(query);
       query = "";
     }
   };
 </script>
 
-<div class="flex gap-4 w-full min-h-[300px] max-h-[300px] max-w-[66vw] min-w-[66vw]">
-  <div class="min-w-[33vw]">
+<CompositionLayout>
+  <svelte:fragment slot="sidebar">
     <Command.Input
       placeholder="Type to search..."
       autofocus
@@ -44,17 +46,23 @@
             on:click={addFact}
           >Add "{query}"</Button>
       </Command.Empty>
-      {#if $facts}
-      {#each $facts as fact (fact.id)}
+      {#if $facts$}
+      {#each $facts$ as fact (fact.id)}
       <Command.Item onSelect={() => handleSelect(fact)}>
         {fact.content}
       </Command.Item>
       {/each}
       {/if}
     </Command.List>
-  </div>
-  <CompositionEditor
-    placeholder={placeholder}
-    bind:value={value}
+  </svelte:fragment>
+
+  <UnstyledTextarea
+    class="w-full"
+    {placeholder}
+    bind:value
   />
-</div>
+
+  <div class="flex justify-end">
+    <Button on:click={() => navigator.clipboard.writeText(value)}>Copy</Button>
+  </div>
+</CompositionLayout>
---
+++ packages/fact-composer/src/lib/composer/CompositionLayout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { cn } from "$lib/utils";
+</script>
+
+<div class="flex gap-4 w-full min-h-[300px] max-h-[300px] max-w-[66vw] min-w-[66vw]">
+  <div class="max-w-[28vw] min-w-[28vw]">
+    <slot name="sidebar"></slot>
+  </div>
+  <div
+    class={cn(
+      "w-full min-h-[300px] max-h-[300px] max-w-[33vw] min-w-[33vw]",
+      "focus-within:border"
+    )}
+  >
+    <slot></slot>
+  </div>
+</div>
