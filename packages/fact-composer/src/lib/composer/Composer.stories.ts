b67960b596dc516244065761161d91873259f0fe
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-06 19:37:47 +0530

    chore(composer): use memory adapter with seed data

--- packages/fact-composer/src/lib/composer/Composer.stories.ts
+++ packages/fact-composer/src/lib/composer/Composer.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/svelte';
 import Composer from './Composer.svelte';
-import { FactsORM, LocalAdapter, type Fact } from '@repo/facts-db';
+import { ORM, MemoryAdapter, type InsertObject } from '@repo/facts-db';
 
 const meta = {
   title: 'features/Composer',
@@ -14,36 +14,31 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const orm = new FactsORM(new LocalAdapter("facts"));
+const facts = new ORM(new MemoryAdapter());
 
-// const workExperiences: Omit<Fact, "id">[] = [
-//   {
-//     content: "Collaborated with offshore technology leadership to develop a new product",
-//   },
-//   {
-//     content: "A hands-on front-end heavy software engineer with about 9 years of professional experience and over a decade of software development expertise.",
-//   },
-//   {
-//     content: "Lead a team of 4 engineers to deliver a new feature",
-//   },
-//   {
-//     content: "Passion and proficiency in Front-end architecture, User Experience design and Cloud-native infrastructure.",
-//   },
-//   {
-//     content: "Developed a new feature for a large-scale web application",
-//   },
-//   {
-//     content: "Strong ability to adapt and adopt best practices in software development.",
-//   }
-// ]
-
-
-// for (const workExperience of workExperiences) {
-//   orm.objects.addOne(workExperience);
-// }
-
-export const _Composer: Story = {
-  args: {
-    db: orm,
+const workExperiences: InsertObject<"facts">[] = [
+  {
+    content: "Collaborated with offshore technology leadership to develop a new product",
   },
-};
+  {
+    content: "A hands-on front-end heavy software engineer with about 9 years of professional experience and over a decade of software development expertise.",
+  },
+  {
+    content: "Lead a team of 4 engineers to deliver a new feature",
+  },
+  {
+    content: "Passion and proficiency in Front-end architecture, User Experience design and Cloud-native infrastructure.",
+  },
+  {
+    content: "Developed a new feature for a large-scale web application",
+  },
+  {
+    content: "Strong ability to adapt and adopt best practices in software development.",
+  }
+]
+
+for (const workExperience of workExperiences) {
+  facts.objects.addOne(workExperience);
+}
+
+export const _Composer: Story = {args: {facts}};
