30fd179ea8d85eb2b5036ffee0d7c4ec0ce339de
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-07 00:42:26 +0530

    refactor(extension): adopt upstream refactors

--- packages/web-extension/src/entrypoints/background.ts
+++ packages/web-extension/src/entrypoints/background.ts
@@ -1,6 +1,18 @@
-import {createRouter} from "@repo/runtime-messaging";
+import {DexieAdapter} from "@repo/facts-db-adapter";
+import {ORM, createFactsDB} from "@repo/facts-db";
+import {Router, createRequestHandlers} from "@repo/runtime-messaging";
 
-const reply = createRouter();
+const db = createFactsDB("facts");
+const facts = new ORM(new DexieAdapter(db, "facts"));
+const handlers = createRequestHandlers(facts);
+
+// TODO: this is an ugly workaround to make the types work
+const reply: Router<"facts"> = (message, sendResponse) => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handler = handlers[message.type] as Function;
+  // eslint-disable-next-line no-useless-call
+  handler?.call(undefined, message.payload, sendResponse);
+};
 
 export default defineBackground(() => {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
