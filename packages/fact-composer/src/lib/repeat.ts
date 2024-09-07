273c32a0e5aa4c2808f42d5cb5fd440c2fadc357
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-07 22:27:56 +0530

    Remove unused files

--- packages/fact-composer/src/lib/repeat.ts
+++
@@ -1,8 +0,0 @@
-import {sleep} from "./sleep";
-
-export const repeat = async (n: number, f: (i: number) => void): Promise<void> => {
-  for (let i = 0; i < n; i++) {
-    f(i);
-    sleep(1000);
-  }
-}
