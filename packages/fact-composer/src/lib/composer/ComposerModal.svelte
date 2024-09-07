5eef169c53ae0fd1eb8ae500ef68d37da3acd133
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-07 18:06:16 +0530

    refactor(fact-composer): update ComposerModal.svelte
    - Update import statements for rxjs and InputElement
    - Remove unused code and event listeners
    - Add appendToHookedElement function to handle composition events

--- packages/fact-composer/src/lib/composer/ComposerModal.svelte
+++ packages/fact-composer/src/lib/composer/ComposerModal.svelte
@@ -1,60 +1,39 @@
 <script lang="ts">
   import "../../app.css";
-  import {isModalOpen} from '$stores/isModalOpen';
-  import {element, type InputElement} from '$stores/element';
-  import { onMount } from "svelte";
   import CommandDialog from '$lib/components/ui/command/command-dialog.svelte';
   import Composition from './Composition.svelte';
   import type { ORM } from '@repo/facts-db';
+  import type { Observable } from "rxjs";
+  import type {InputElement} from "$lib/InputElement";
+  import { concat } from "$lib/concat";
 
   export let facts: ORM<"facts">;
+  export let hookElement$: Observable<InputElement>;
 
   let open = false;
   let placeholder = "";
-  let inputs: HTMLInputElement[] = [];
-  let textareas: HTMLTextAreaElement[] = [];
+  $: $hookElement$ && (open = true);
 
-  const openModal = (e: KeyboardEvent) => {
-    if (e.ctrlKey && e.key === ".") {
-      isModalOpen.set(true);
-      open = true;
-      element.set(e.target as InputElement);
+  const appendToHookedElement = (str: string) => {
+    if ($hookElement$) {
+      $hookElement$.value = concat("\n")($hookElement$.value, str);
+      open = false;
     }
   };
 
-  const removeListeners = () => {
-    for (const input of inputs) {
-      input.removeEventListener("keydown", openModal);
-    }
-    for (const textarea of textareas) {
-      textarea.removeEventListener("keydown", openModal);
-    }
-  };
-
-  const addListeners = () => {
-    inputs = Array.from(document.querySelectorAll("input") ?? []);
-    textareas = Array.from(document.querySelectorAll("textarea") ?? []);
-    for (const input of inputs) {
-      input.addEventListener("keydown", openModal);
-    }
-    for (const textarea of textareas) {
-      textarea.addEventListener("keydown", openModal);
-    }
-  };
-
-  onMount(() => {
-    addListeners();
-    return removeListeners;
-  });
 </script>
 
 
 <CommandDialog
   class="min-w-[66vw]"
-  closeFocus={$element}
+  closeFocus={$hookElement$}
   closeOnOutsideClick={false}
   bind:value={placeholder}
   bind:open
 >
-  <Composition {facts} {placeholder} />
+  <Composition
+    {facts}
+    {placeholder}
+    on:composition={({detail}) => appendToHookedElement(detail)}
+  />
 </CommandDialog>
