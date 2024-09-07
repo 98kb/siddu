f7b64905fbd5d45b4987042a60c4125e585fedca
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-07 12:52:47 +0530

    refactor(web-extension): refactor background entrypoint
    Refactor the background entrypoint in the web extension to improve code organization and readability. Move the message handling logic to separate functions for adding context menus and messaging for the facts database. This improves maintainability and makes the code more modular.

--- packages/web-extension/src/entrypoints/background.ts
+++ packages/web-extension/src/entrypoints/background.ts
@@ -1,22 +1,7 @@
-import {DexieAdapter} from "@repo/facts-db-adapter";
-import {ORM, createFactsDB} from "@repo/facts-db";
-import {Router, createRequestHandlers} from "@repo/runtime-messaging";
-
-const db = createFactsDB("facts");
-const facts = new ORM(new DexieAdapter(db, "facts"));
-const handlers = createRequestHandlers(facts);
-
-// TODO: this is an ugly workaround to make the types work
-const reply: Router<"facts"> = (message, sendResponse) => {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  const handler = handlers[message.type] as Function;
-  // eslint-disable-next-line no-useless-call
-  handler?.call(undefined, message.payload, sendResponse);
-};
+import {addContextMenus} from "./background/addContextMenus";
+import {addFactsDbMessaging} from "./background/addFactsDbMessaging";
 
 export default defineBackground(() => {
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    reply(message, sendResponse);
-    return true;
-  });
+  addFactsDbMessaging();
+  addContextMenus();
 });
