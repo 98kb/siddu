1b194ae5efad2e5a62c0a312fdc2627b03701702
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-07 18:00:26 +0530

    refactor(fact-composer): remove sample stories

--- packages/web-extension/src/entrypoints/composer-modal.content/index.ts
+++ packages/web-extension/src/entrypoints/composer-modal.content/index.ts
@@ -5,10 +5,17 @@ import {ComposerModal} from "@repo/fact-composer";
 import {ORM} from "@repo/facts-db";
 import {RuntimeAdapter} from "@repo/facts-db-adapter";
 
+// TODO: Create a wrapper for ComposerModal that will handle focus and keyboard events
 export default defineContentScript({
   matches: ["<all_urls>"],
   cssInjectionMode: "ui",
   async main(ctx) {
+    ctx.addEventListener(document, "focus", e => {
+      console.log("here", e);
+    });
+    ctx.addEventListener(document, "focusin", e => {
+      console.log("here", e);
+    });
     const ui = await createShadowRootUi(ctx, {
       name: "fact-composer",
       position: "modal",
