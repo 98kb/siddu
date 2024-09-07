adb3fbe8f9bd76efc299de70bc87c83b63c3396a
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-07 18:09:34 +0530

    refactor(composer-modal): use composer modal factory

--- packages/web-extension/src/entrypoints/composer-modal.content/index.ts
+++ packages/web-extension/src/entrypoints/composer-modal.content/index.ts
@@ -1,35 +1,19 @@
 import "@repo/fact-composer/dist/style.css";
 // eslint-disable-next-line sort-imports-es6-autofix/sort-imports-es6
 import "./style.css";
-import {ComposerModal} from "@repo/fact-composer";
-import {ORM} from "@repo/facts-db";
-import {RuntimeAdapter} from "@repo/facts-db-adapter";
+import {createComposerModal} from "./createComposerModal";
 
 // TODO: Create a wrapper for ComposerModal that will handle focus and keyboard events
 export default defineContentScript({
   matches: ["<all_urls>"],
   cssInjectionMode: "ui",
   async main(ctx) {
-    ctx.addEventListener(document, "focus", e => {
-      console.log("here", e);
-    });
-    ctx.addEventListener(document, "focusin", e => {
-      console.log("here", e);
-    });
     const ui = await createShadowRootUi(ctx, {
       name: "fact-composer",
       position: "modal",
       isolateEvents: true,
       mode: "closed",
-      onMount: container => {
-        container.style.zIndex = "999999999";
-        return new ComposerModal({
-          target: container,
-          props: {
-            facts: new ORM(new RuntimeAdapter()),
-          },
-        });
-      },
+      onMount: createComposerModal,
       onRemove: app => {
         app?.$destroy();
       },
