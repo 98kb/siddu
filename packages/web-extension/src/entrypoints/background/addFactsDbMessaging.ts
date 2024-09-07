228325115ae376ea3e9080ed4e003d650ec0c144
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-07 12:52:14 +0530

    feat(web-extension): add messaging for facts database
    This commit adds the necessary code to enable messaging between the web extension and the facts database. It sets up the necessary imports, creates the facts database instance, and defines the request handlers. Additionally, it includes a workaround to make the types work correctly. The messaging functionality is implemented using the `chrome.runtime.onMessage` listener.

---
+++ packages/web-extension/src/entrypoints/background/addFactsDbMessaging.ts
@@ -0,0 +1,23 @@
+import {DexieAdapter} from "@repo/facts-db-adapter";
+import {ORM, createFactsDB} from "@repo/facts-db";
+import {Router, createRequestHandlers} from "@repo/runtime-messaging";
+
+const db = createFactsDB("facts");
+const facts = new ORM(new DexieAdapter(db, "facts"));
+const handlers = createRequestHandlers(facts);
+
+// TODO: this is an ugly workaround to make the types work
+const reply: Router<"facts"> = (message, sendResponse) => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handler = handlers[message.type] as Function;
+  // eslint-disable-next-line no-useless-call
+  handler?.call(undefined, message.payload, sendResponse);
+};
+
+export const addFactsDbMessaging = () => {
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("message", message);
+    reply(message, sendResponse);
+    return true;
+  });
+};
