40d850daefa9c23dd6a4ea1fe6b5e10023ec4343
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-07 18:02:33 +0530

    refactor(fact-composer): update LocalAdapter import location

--- packages/fact-composer/src/main.ts
+++ packages/fact-composer/src/main.ts
@@ -1,6 +1,7 @@
-import { ORM, LocalAdapter } from '@repo/facts-db';
+import { ORM } from '@repo/facts-db';
 import './app.css';
 import App from './App.svelte';
+import { LocalAdapter } from '@repo/facts-db-adapter';
 
 const app = new App({
   target: document.getElementById('app')!,
