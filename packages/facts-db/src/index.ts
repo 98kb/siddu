78372ea73b58c5738347b2d23afea29ff07198cc
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-11 17:26:52 +0530

    feat(facts-db): expose InsertFact and TableSchemas types

--- packages/facts-db/src/index.ts
+++ packages/facts-db/src/index.ts
@@ -2,4 +2,7 @@ export * from "./as$";
 export * from "./createFactsDB";
 export * from "./fact/Fact";
 export * from "./fact/FactSchema";
+export * from "./fact/InsertFact";
 export * from "./fact/InsertFactSchema";
+export * from "./TableSchemas";
+export * from "./Tables";
