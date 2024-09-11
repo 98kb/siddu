a222fc487f271c326090d5050e65df6882ebcfb5
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-11 18:19:41 +0530

    refactor(facts-service): update FactsService constructor
    Refactor the FactsService constructor to accept an AdapterFactory parameter instead of a generic factory function. This change improves the clarity and maintainability of the code.

--- packages/facts-service/src/FactsService.ts
+++ packages/facts-service/src/FactsService.ts
@@ -1,12 +1,10 @@
+import {AdapterFactory} from "./AdapterFactory";
 import {IAdapter} from "./IAdapter";
-import {Tables} from "@repo/facts-db";
 
 export class FactsService {
   readonly facts: IAdapter<"facts">;
 
-  constructor(
-    private factory: <T extends keyof Tables>(tableName: T) => IAdapter<T>,
-  ) {
-    this.facts = this.factory("facts");
+  constructor(factory: AdapterFactory) {
+    this.facts = factory("facts");
   }
 }
