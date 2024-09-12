8e5b17dbb5f4debb40af8283f0f0fe80bdc8977e
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-12 17:37:20 +0530

    chore(fact-composer): update vite.config.ts
    Refactor the vite.config.ts file in the fact-composer package.
    - Update import statements to use double quotes instead of single quotes.
    - Update path import for the 'path' module to use the full node:path import.
    - Update the lib entry name to 'FactComposer'.
    - Update the lib fileName function to use template literals for better readability.
    - Update the resolve alias paths to use the full path instead of relative paths.

--- packages/fact-composer/vite.config.ts
+++ packages/fact-composer/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import typescript from '@rollup/plugin-typescript';
-import path from 'path';
+import {defineConfig} from "vite";
+import {svelte} from "@sveltejs/vite-plugin-svelte";
+import path from "node:path";
+import typescript from "@rollup/plugin-typescript";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,15 +9,15 @@ export default defineConfig({
   build: {
     sourcemap: true,
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'SearchModal',
-      fileName: (format) => `fact-composer.${format}.js`,
-    }
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "FactComposer",
+      fileName: format => `fact-composer.${format}.js`,
+    },
   },
   resolve: {
     alias: {
-      $lib: path.resolve('./src/lib'),
-      $ui: path.resolve('./src/lib/components/ui'),
-    }
-  }
-})
+      $lib: path.resolve("./src/lib"),
+      $ui: path.resolve("./src/lib/components/ui"),
+    },
+  },
+});
