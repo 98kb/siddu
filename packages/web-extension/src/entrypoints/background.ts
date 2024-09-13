13c27a204148b5159bb3e141cda8e62734b52416
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-14 01:59:59 +0530

    feat(web-extension): remove unused addFactsDbMessaging import in background script

--- packages/web-extension/src/entrypoints/background.ts
+++ packages/web-extension/src/entrypoints/background.ts
@@ -1,9 +1,7 @@
 import {addContextMenus} from "./background/addContextMenus";
 import {createChromeRuntimeServer} from "@repo/facts-service-trpc";
-// import {addFactsDbMessaging} from "./background/addFactsDbMessaging";
 
 export default defineBackground(() => {
-  // addFactsDbMessaging();
   addContextMenus();
   createChromeRuntimeServer();
 });
