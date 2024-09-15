71dfd63dae7f8052bb38a09898964187e7d70a4e
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-15 13:04:26 +0530

    test(adapters): enhance tests for TRPCService with server lifecycle management

--- packages/facts-service/tests/adapters.test.ts
+++ packages/facts-service/tests/adapters.test.ts
@@ -1,7 +1,10 @@
 import {DexieAdapter} from "../src/DexieAdapter";
 import {MemoryAdapter} from "../src/MemoryAdapter";
+import {Server} from "node:http";
+import {TRPCService} from "../src/TRPCService";
 import {Tables, createFactsDB} from "@repo/facts-db";
-import {beforeEach, describe} from "vitest";
+import {afterAll, beforeAll, beforeEach, describe} from "vitest";
+import {createTestClient, createTestServer} from "./util/createTestServer";
 import {describeAdapter} from "./describeAdapter";
 
 type TableNames = keyof Tables;
@@ -9,13 +12,25 @@ const tableNames = ["facts"] satisfies TableNames[];
 
 for (const tableName of tableNames) {
   const adapters = [
+    new TRPCService(tableName, createTestClient()),
     new DexieAdapter(tableName, createFactsDB("test")),
     new MemoryAdapter(tableName),
   ];
 
   for (const adapter of adapters) {
     describe(adapter.constructor.name, () => {
-      beforeEach(async () => await adapter.deleteAll());
+      if (adapter instanceof TRPCService) {
+        let server: Server;
+        beforeAll(async () => {
+          server = await createTestServer();
+        });
+        afterAll(() => {
+          server.close();
+        });
+      }
+      beforeEach(async () => {
+        await adapter.deleteAll();
+      });
       describeAdapter(adapter);
     });
   }
