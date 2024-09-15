5c1f3d2620156425282daec519139920196d9681
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-15 13:04:11 +0530

    add unit test for on mutation hook

--- packages/facts-service/tests/features/describeOnMutation.ts
+++ packages/facts-service/tests/features/describeOnMutation.ts
@@ -1,19 +1,32 @@
 import {IAdapter} from "../../src";
 import {Tables} from "@repo/facts-db";
-import {expect, it} from "vitest";
+import {it} from "vitest";
 
 export const describeOnMutation = <T extends keyof Tables>(
   adapter: IAdapter<T>,
 ) => {
-  it("emits on every mutation", async () => {
-    let mutationCount = 0;
-    const sub = adapter.onMutation(() => mutationCount++);
-    const actions = [
-      await adapter.add({content: "test"}),
-      await adapter.add({content: "test"}),
-      await adapter.deleteAll(),
-    ];
-    sub.unsubscribe();
-    expect(mutationCount).toEqual(actions.length);
-  });
+  // TODO: Fix flaky test
+  /**
+   * This is a flaky test. Sometimes it never resolves.
+   */
+  it("emits on every mutation", () =>
+    new Promise<void>(resolve => {
+      let mutationCount = 0;
+      const actions = [
+        () => adapter.add({content: "test"}),
+        () => adapter.deleteAll(),
+      ];
+
+      const sub = adapter.onMutation(() => {
+        mutationCount++;
+        if (mutationCount === actions.length) {
+          sub.unsubscribe();
+          resolve();
+        }
+      });
+
+      for (const action of actions) {
+        action();
+      }
+    }));
 };
