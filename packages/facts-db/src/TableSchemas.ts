f16ab3662f25d00f906423a70461a499736dca2f
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-17 22:59:30 +0530

    feat(facts-db): add Collection and InsertCollection schema

--- packages/facts-db/src/TableSchemas.ts
+++ packages/facts-db/src/TableSchemas.ts
@@ -1,9 +1,15 @@
-import {Fact} from "./fact/Fact";
-import {InsertFact} from "./fact/InsertFact";
+import {Collection} from "./tables/collection/Collection";
+import {Fact} from "./tables/fact/Fact";
+import {InsertCollection} from "./tables/collection/InsertCollection";
+import {InsertFact} from "./tables/fact/InsertFact";
 
 export type TableSchemas = {
   facts: {
     schema: Fact;
     insertSchema: InsertFact;
+  };
+  collections: {
+    schema: Collection;
+    insertSchema: InsertCollection;
   };
 };
