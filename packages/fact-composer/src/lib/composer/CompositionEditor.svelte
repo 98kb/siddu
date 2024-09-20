a0d5d0c9265ada0ca5924774c6fbf17ad752f46b
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-20 21:52:53 +0530

    feat(fact-composer): remove copy button from editor

--- packages/fact-composer/src/lib/composer/CompositionEditor.svelte
+++ packages/fact-composer/src/lib/composer/CompositionEditor.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
-  import Button from "$ui/button/button.svelte";
   import {composition} from "./store/composition";
   import UnstyledTextarea from "$lib/components/ui/textarea/UnstyledTextarea.svelte";
-
-  const copyValue = () => navigator.clipboard.writeText($composition);
 </script>
 
 <div class="flex justify-end">
-  <Button on:click={copyValue}>Copy</Button>
   <slot />
 </div>
 
