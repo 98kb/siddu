067fcb1bcf12f7c2dfa61dde6421ad82313995a3
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-22 00:17:25 +0530

    feat(fact-composer): integrate with router and add stub tabs

--- packages/fact-composer/src/lib/nav/Navbar.svelte
+++ packages/fact-composer/src/lib/nav/Navbar.svelte
@@ -1,19 +1,68 @@
 <script lang="ts">
   import {Button} from "$lib/components/ui/button";
-  import {PencilRulerIcon, HomeIcon, NotebookIcon, TagIcon} from "lucide-svelte";
+  import {activeRoute, setActiveRoute} from "$lib/router/router";
+  import {
+    PencilRulerIcon,
+    HomeIcon,
+    NotebookIcon,
+    TagIcon,
+    ShoppingBagIcon,
+    WrenchIcon,
+    UserCircleIcon,
+  } from "lucide-svelte";
+
+  const tabs = [
+    {
+      name: "composition",
+      icon: PencilRulerIcon,
+    },
+    {
+      name: "collections",
+      icon: NotebookIcon,
+    },
+    {
+      name: "labels",
+      icon: TagIcon,
+    },
+    {
+      name: "marketplace",
+      icon: ShoppingBagIcon,
+    },
+  ] as const;
+
+  const bottomTabs = [
+    {
+      name: "settings",
+      icon: WrenchIcon,
+    },
+    {
+      name: "account",
+      icon: UserCircleIcon,
+    }
+  ] as const;
 </script>
 
 <div class="flex flex-col min-w-14 h-full shadow items-center py-2 gap-4">
   <Button size="icon" variant="ghost">
     <HomeIcon />
   </Button>
-  <Button size="icon" variant="default">
-    <PencilRulerIcon />
-  </Button>
-  <Button size="icon" variant="ghost">
-    <NotebookIcon />
-  </Button>
-  <Button size="icon" variant="ghost">
-    <TagIcon />
-  </Button>
+  {#each tabs as { name, icon }}
+    <Button
+      size="icon"
+      variant={$activeRoute === name ? "default" : "ghost"}
+      on:click={() => setActiveRoute(name)}
+    >
+      <svelte:component this={icon} />
+    </Button>
+  {/each}
+  <div class="grow"></div>
+  {#each bottomTabs as { name, icon }}
+    <Button
+      size="icon"
+      variant={$activeRoute === name ? "default" : "ghost"}
+      on:click={() => setActiveRoute(name)}
+    >
+      <svelte:component this={icon} />
+    </Button>
+  {/each}
 </div>
