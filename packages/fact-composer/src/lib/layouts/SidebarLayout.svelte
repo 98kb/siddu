0c160417768f7c9314dd59444300c809a47f6730
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-22 00:14:16 +0530

    feat(fact-composer): add SidebarLayout component

---
+++ packages/fact-composer/src/lib/layouts/SidebarLayout.svelte
@@ -0,0 +1,8 @@
+<div class="flex h-full gap-4">
+  <div class="w-full h-full bg-gray-50">
+    <slot name="sidebar"></slot>
+  </div>
+  <div class="flex flex-col w-full h-full">
+    <slot></slot>
+  </div>
+</div>
