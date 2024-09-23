5b6e3da3e19429fceec77b3414bc64d9d4fb1834
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-23 17:17:10 +0530

    feat(fact-composer): add SearchInput component

---
+++ packages/fact-composer/src/lib/components/SearchInput.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import {SearchIcon} from "lucide-svelte";
+
+  export let value: string;
+</script>
+
+<div class="flex items-center border px-3 py-3 gap-3">
+  <SearchIcon size={16} class="text-foreground" />
+  <input
+    type="text"
+    class="w-full focus:outline-none text-sm text-foreground"
+    placeholder="Type to search..."
+    bind:value
+  />
+</div>
