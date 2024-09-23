741330523a9c4053e76e5ad9a7758beaeae95748
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-23 17:17:17 +0530

    feat(fact-composer): add SearchInput component

---
+++ packages/fact-composer/src/lib/components/SearchInput.stories.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import {Meta, Story} from "@storybook/addon-svelte-csf";
+  import SearchInput from "./SearchInput.svelte";
+
+  let value = "";
+
+</script>
+
+<Meta
+  autodocs
+  title="features/search/SearchInput"
+  component={SearchInput}
+  tags={["autodocs"]}
+  parameters={{layout: "centered"}}
+/>
+
+<Story
+  name="Search Input"
+  let:args
+>
+  <SearchInput {...args} bind:value />
+</Story>
