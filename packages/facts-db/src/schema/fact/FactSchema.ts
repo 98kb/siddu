0ba3fa6b84e68edb4a2cf7b16c61adbcb3539de6
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-23 14:00:53 +0530

    refactor(facts-db): add optional labels to FactSchema

--- packages/facts-db/src/schema/fact/FactSchema.ts
+++ packages/facts-db/src/schema/fact/FactSchema.ts
@@ -1,7 +1,9 @@
 import {InsertFactSchema} from "./InsertFactSchema";
+import {LabelSchema} from "../label/LabelSchema";
 import {z} from "zod";
 
 export const FactSchema = z.object({
   id: z.number(),
+  labels: z.array(LabelSchema).optional(),
   ...InsertFactSchema.shape,
 });
