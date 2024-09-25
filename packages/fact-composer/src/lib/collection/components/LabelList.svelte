66eba2aaffa2c0015f94d7516a54fb2f601fbd96
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-25 19:56:17 +0530

    feat(fact-composer): add archive nav item

--- packages/fact-composer/src/lib/collection/components/LabelList.svelte
+++ packages/fact-composer/src/lib/collection/components/LabelList.svelte
@@ -2,15 +2,19 @@
   import AsidePill from "./AsidePill.svelte";
   import LabelIcon from "./LabelIcon.svelte";
   import type {Label} from "@repo/facts-db";
-  import {BookOpenTextIcon} from "lucide-svelte";
+  import {ArchiveIcon, BookOpenTextIcon} from "lucide-svelte";
 
   export let labels: Label[];
   let selected: number = 0;
 </script>
 
-<AsidePill isSelected={selected === 0} onClick={() => (selected = 0)}>
+<AsidePill isSelected={selected === -1} onClick={() => (selected = -1)}>
   <BookOpenTextIcon />
   Facts
+</AsidePill>
+<AsidePill isSelected={selected === 0} onClick={() => (selected = 0)}>
+  <ArchiveIcon />
+  Archive
 </AsidePill>
 {#each labels as label (label.id)}
   <AsidePill
