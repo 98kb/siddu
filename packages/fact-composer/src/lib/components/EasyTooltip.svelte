8ed1dbab1c191b7347565be67e26b7e891e3d0cd
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-26 16:10:48 +0530

    feat(fact-composer): add EasyTooltip component

---
+++ packages/fact-composer/src/lib/components/EasyTooltip.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type {ComponentProps} from "svelte";
+  import {Tooltip, TooltipTrigger} from "./ui/tooltip";
+  import TooltipContent from "./ui/tooltip/tooltip-content.svelte";
+
+  export let tooltip: string;
+  export let side: ComponentProps<TooltipContent>["side"] = "bottom";
+</script>
+
+<Tooltip openDelay={500}>
+  <TooltipTrigger>
+    <slot />
+  </TooltipTrigger>
+  <TooltipContent {side} sideOffset={0}>
+    <span class="capitalize text-xs">{tooltip}</span>
+  </TooltipContent>
+</Tooltip>
