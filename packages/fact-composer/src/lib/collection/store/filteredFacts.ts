ce5011202950e72ea5ccf50311267affece9806f
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-28 17:36:17 +0530

    refactor(fact-composer): optimize filtering of facts by labels

--- packages/fact-composer/src/lib/collection/store/filteredFacts.ts
+++ packages/fact-composer/src/lib/collection/store/filteredFacts.ts
@@ -1,19 +1,11 @@
 import {derived} from "svelte/store";
 import {facts} from "$lib/store/facts";
 import {factsFilter} from "./factsFilter";
+import {filterFactsByLabels} from "../filterFactsByLabels";
 
 export const filteredFacts = derived(
   [facts, factsFilter],
   ([$facts, $filter]) => {
-    if ($filter.labels.length === 0) {
-      return $facts;
-    }
-    const matchEveryFilterLabel = (factLabelIds: number[]) =>
-      $filter.labels
-        .map(({id}) => id)
-        .every(filterLabelId => factLabelIds.includes(filterLabelId));
-    return $facts.filter(fact =>
-      matchEveryFilterLabel(fact.labels.map(({id}) => id)),
-    );
+    return filterFactsByLabels($facts, $filter.labels);
   },
 );
