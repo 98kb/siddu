6b5e9b5f7ea956cc220ddb2c791fcb91307699fe
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-10-01 00:12:39 +0530

    feat(fact-composer): remove layout from fact cards

--- packages/fact-composer-app/src/collection/components/FactCards.tsx
+++ packages/fact-composer-app/src/collection/components/FactCards.tsx
@@ -1,5 +1,4 @@
 import {Fact, Label} from "@repo/facts-db";
-import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
 import {FactCard} from "./FactCard";
 
 type TProps = {
@@ -13,22 +12,14 @@ export function FactCards({
   highlightedFacts,
   highlightedLabels,
 }: TProps) {
-  return (
-    <ResponsiveMasonry
-      columnsCountBreakPoints={{320: 1, 400: 2, 750: 4, 900: 5}}
-    >
-      <Masonry gutter="0.5rem">
-        {facts.map(fact => (
-          <FactCard
-            key={fact.id}
-            fact={fact}
-            isHighlighted={isHighlighted(fact, highlightedFacts)}
-            highlightedLabels={highlightedLabels}
-          />
-        ))}
-      </Masonry>
-    </ResponsiveMasonry>
-  );
+  return facts.map(fact => (
+    <FactCard
+      key={fact.id}
+      fact={fact}
+      isHighlighted={isHighlighted(fact, highlightedFacts)}
+      highlightedLabels={highlightedLabels}
+    />
+  ));
 }
 
 function isHighlighted(fact: Fact, highlightedFacts?: Fact[]) {
