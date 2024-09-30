e81d10693088ed3b737edcc70019a5b91433b894
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-30 09:49:45 +0530

    feat(fact-composer): add memory router

--- packages/fact-composer-app/src/composer/ComposerModal.tsx
+++ packages/fact-composer-app/src/composer/ComposerModal.tsx
@@ -3,6 +3,7 @@ import {DbClient} from "@repo/facts-db";
 import {ComposerNav} from "./components/ComposerNav";
 import {useState} from "react";
 import {TooltipProvider} from "~/components/ui/tooltip";
+import {MemoryRouter, Route, Routes} from "react-router-dom";
 
 type TProps = {
   db?: DbClient;
@@ -13,12 +14,19 @@ export function ComposerModal({db}: TProps) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <TooltipProvider>
-      <Dialog modal open={isOpen} onOpenChange={setIsOpen}>
-        <DialogContent className="flex gap-0 h-full rounded-lg overflow-y-scroll overflow-x-visible p-0 min-w-[70vw] min-h-[600px] max-h-[600px] max-w-[70vw]">
-          <ComposerNav />
-        </DialogContent>
-      </Dialog>
-    </TooltipProvider>
+    <MemoryRouter>
+      <TooltipProvider>
+        <Dialog modal open={isOpen} onOpenChange={setIsOpen}>
+          <DialogContent className="flex gap-0 h-full rounded-lg overflow-y-scroll overflow-x-visible p-0 min-w-[70vw] min-h-[600px] max-h-[600px] max-w-[70vw]">
+            <ComposerNav />
+            <Routes>
+              <Route path="collection" element={<div>Collection</div>} />
+              <Route path="composer" element={<div>Composer</div>} />
+              <Route path="labels" element={<div>Labels</div>} />
+            </Routes>
+          </DialogContent>
+        </Dialog>
+      </TooltipProvider>
+    </MemoryRouter>
   );
 }
