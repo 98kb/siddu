36601b149762140e17d0f1cd78ec607cbb23fc03
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-30 19:22:09 +0530

    chore(fact-composer): add memory router decorator

--- packages/fact-composer-app/.storybook/preview.tsx
+++ packages/fact-composer-app/.storybook/preview.tsx
@@ -1,8 +1,17 @@
+import React from "react";
 import type {Preview} from "@storybook/react";
 import "../src/index.css";
+import {MemoryRouter} from "react-router-dom";
 
 const preview: Preview = {
   tags: ["autodocs"],
+  decorators: [
+    Story => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
   parameters: {
     layout: "centered",
     controls: {
