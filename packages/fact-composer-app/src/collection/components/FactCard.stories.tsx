f72ec7ff4fabc2b0964ef57e3a71595c84a8515e
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-09-30 23:30:26 +0530

    feat(fact-composer): add FactCard component

---
+++ packages/fact-composer-app/src/collection/components/FactCard.stories.tsx
@@ -0,0 +1,57 @@
+import type {Meta, StoryObj} from "@storybook/react";
+import {fn} from "@storybook/test";
+
+import {FactCard} from "./FactCard";
+import {Fact} from "@repo/facts-db";
+
+const meta = {
+  title: "features/collection/components/FactCard",
+  component: FactCard,
+  args: {
+    onClick: fn(),
+  },
+} satisfies Meta<typeof FactCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const fact: Fact = {
+  id: 0,
+  content:
+    "This is a really long fact that is going to be truncated because it is too long to fit in the card.",
+  labels: [
+    {
+      id: 0,
+      name: "ChatGPT",
+    },
+    {
+      id: 1,
+      name: "GPT-3",
+    },
+    {
+      id: 2,
+      name: "GoT",
+    },
+    {
+      id: 3,
+      name: "WorkExp",
+    },
+  ],
+};
+
+export const Default: Story = {
+  args: {
+    fact,
+    isHighlighted: false,
+  },
+};
+
+export const WithHighlightedLabels: Story = {
+  args: {
+    fact,
+    highlightedLabels: [
+      {id: 0, name: "ChatGPT"},
+      {id: 2, name: "GoT"},
+    ],
+  },
+};
---
+++ packages/fact-composer-app/src/collection/components/FactCard.tsx
@@ -0,0 +1,67 @@
+import {Fact, Label} from "@repo/facts-db";
+import {Card, CardContent, CardFooter} from "~/components/ui/card";
+import {cn} from "~/lib/utils";
+import {LabelPills} from "../labels/components/LabelPills";
+import {Button} from "~/components/ui/button";
+import {ArchiveXIcon} from "lucide-react";
+import {EasyTooltip} from "~/components/EasyTooltip";
+import {TooltipProvider} from "~/components/ui/tooltip";
+
+type TProps = {
+  fact: Fact;
+  isHighlighted?: boolean;
+  highlightedLabels?: Label[];
+  onClick?: () => void;
+};
+
+export function FactCard({
+  fact,
+  highlightedLabels,
+  isHighlighted,
+  onClick,
+}: TProps) {
+  return (
+    <TooltipProvider>
+      <Card
+        className={cn([
+          "max-w-[200px] min-w-[200px] w-full",
+          "group",
+          "select-none",
+          "shadow-none hover:shadow hover:border-gray-400",
+          "transition-shadow duration-100 ease-in-out",
+          {"border-black hover:border-black": isHighlighted},
+        ])}
+        onClick={onClick}
+      >
+        <CardContent>
+          <div className="w-full">
+            <p className="min-h-[100px] line-clamp-5 cursor-default pt-4 mb-2">
+              {fact.content}
+            </p>
+            <div className="flex gap-1 flex-wrap">
+              <LabelPills
+                labels={fact.labels}
+                highlightedLabels={highlightedLabels}
+              />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className="opacity-0 pb-4 group-hover:opacity-100">
+          <FactCardActions />
+        </CardFooter>
+      </Card>
+    </TooltipProvider>
+  );
+}
+
+function FactCardActions() {
+  return (
+    <div className="flex gap-2">
+      <EasyTooltip side="bottom" tooltip="Archive" openDelay={50}>
+        <Button variant="ghost" size="icon-sm">
+          <ArchiveXIcon />
+        </Button>
+      </EasyTooltip>
+    </div>
+  );
+}
