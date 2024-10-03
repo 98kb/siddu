62afc50291d4a981c9303cf45455a467997093df
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-10-03 10:06:08 +0530

    feat(facts-db): pass unit tests to ensure entity managers fullfil adapter contract

--- packages/facts-db/src/entities/EntityManager.ts
+++ packages/facts-db/src/entities/EntityManager.ts
@@ -2,6 +2,7 @@ import {AdapterOption} from "../adapters/AdapterOption";
 import {DbClient} from "../DbClient";
 import {IAdapter} from "../adapters/IAdapter";
 import {MutationSubscription} from "../adapters/MutationSubscription";
+import {Reader} from "fp-ts/lib/Reader";
 import {TableSchemas} from "../schema/TableSchemas";
 import {Tables} from "../schema/Tables";
 import {TypeOf, z} from "zod";
@@ -39,8 +40,10 @@ export class EntityManager<T extends keyof Tables> implements IAdapter<T> {
     return this.crud.get(id);
   }
 
-  getAll(): Promise<TypeOf<TableSchemas[T]["schema"]>[]> {
-    return this.crud.getAll();
+  getAll(
+    predicate?: Reader<TypeOf<TableSchemas[T]["schema"]>, boolean>,
+  ): Promise<TypeOf<TableSchemas[T]["schema"]>[]> {
+    return this.crud.getAll(predicate);
   }
 
   onMutation(callback: () => void): MutationSubscription {
---
+++ packages/facts-db/tests/entitites-managers/FactsManager.test.ts
@@ -0,0 +1,27 @@
+import {DbClient} from "../../src/DbClient";
+import {FactSchema} from "../../src/schema/fact/FactSchema";
+import {FactsManager} from "../../src/entities/FactsManager";
+import {InsertFactSchema} from "../../src/schema/fact/InsertFactSchema";
+import {afterEach, beforeEach, describe} from "vitest";
+import {createMemoryAdapter} from "../../src/adapters/createMemoryAdapter";
+import {describeAdapter} from "../adapters/describeAdapter";
+
+const db = new DbClient(createMemoryAdapter);
+const createFactManager = () =>
+  new FactsManager(
+    createMemoryAdapter({
+      entity: "facts",
+      schema: FactSchema,
+      insertSchema: InsertFactSchema,
+    }),
+    db,
+  );
+
+describe(FactsManager.name, () => {
+  let manager = createFactManager();
+  beforeEach(() => {
+    manager = createFactManager();
+  });
+  afterEach(() => manager.deleteAll());
+  describeAdapter(manager);
+});
