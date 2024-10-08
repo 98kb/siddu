104d690765db35fbd79156b257acc1f37934f7a1
dsfx3d <dsfx3d@gmail.com>
2024-10-08 12:55:20 +0530

    Refactor FactList.stories.tsx to remove unnecessary code

--- packages/fact-composer-app/src/composition/features/FactList.stories.tsx
+++ packages/fact-composer-app/src/composition/features/FactList.stories.tsx
@@ -30,8 +30,4 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
-  args: {
-    open: true,
-  },
-};
+export const Primary: Story = {};
