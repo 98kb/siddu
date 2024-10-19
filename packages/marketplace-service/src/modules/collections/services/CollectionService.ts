fdc654295aeecec6006f6afeef0fb3adce138f65
dsfx3d <dsfx3d@gmail.com>
2024-10-20 04:15:53 +0530

    feat(marketplace-service): update CollectionService to include type definitions and structured facts

--- packages/marketplace-service/src/modules/collections/services/CollectionService.ts
+++ packages/marketplace-service/src/modules/collections/services/CollectionService.ts
@@ -1,23 +1,50 @@
 import {Collection} from "../models/Collection";
-import {LimitOffset} from "./../../../common/dto/LimitOffsetDto";
+import {
+  LimitOffset,
+  LimitOffsetDto,
+} from "./../../../common/dto/LimitOffsetDto";
 import {ListFactsInput} from "../dto/ListFactsInputDto";
+import {TCollectionSchema} from "../schemas/CollectionSchema";
+import {TFactSchema} from "../schemas/FactSchema";
 
 export class CollectionService {
-  list({limit, offset}: LimitOffset) {
-    return Collection.find()
-      .select("~facts name")
-      .limit(limit)
+  async list({limit, offset}: LimitOffset): Promise<TCollectionSchema[]> {
+    const collections = await Collection.find()
+      .select({facts: false})
+      .limit(Math.min(limit, LimitOffsetDto.shape.limit.maxValue ?? 1))
       .skip(offset)
       .lean();
+    return collections.map(({_id, name}) => ({
+      _id: _id.toString(),
+      name,
+    }));
   }
 
-  get(id: string) {
-    return Collection.findById(id).select("~facts name").lean();
+  async get(id: string): Promise<TCollectionSchema | undefined> {
+    const collection = await Collection.findById(id)
+      .select({facts: false})
+      .lean();
+    return collection
+      ? {
+          _id: collection._id.toString(),
+          name: collection.name,
+        }
+      : undefined;
   }
 
-  listFacts({collectionId, limit, offset}: ListFactsInput) {
-    return Collection.findById(collectionId)
+  async listFacts({
+    collectionId,
+    limit,
+    offset,
+  }: ListFactsInput): Promise<TFactSchema[]> {
+    const collection = await Collection.findById(collectionId)
       .select({facts: {$slice: [offset, limit]}})
       .lean();
+    return (
+      collection?.facts.map(({content, title}) => ({
+        content,
+        title,
+      })) ?? []
+    );
   }
 }
