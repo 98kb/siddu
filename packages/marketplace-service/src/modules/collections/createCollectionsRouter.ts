fb935d40bc42a06af34721cb30f6fc42898ac85e
dsfx3d <dsfx3d@gmail.com>
2024-10-20 04:16:11 +0530

    feat(marketplace-service): update createCollectionsRouter to include CollectionSchema and FactSchema

--- packages/marketplace-service/src/modules/collections/createCollectionsRouter.ts
+++ packages/marketplace-service/src/modules/collections/createCollectionsRouter.ts
@@ -1,6 +1,9 @@
+import {CollectionSchema} from "./schemas/CollectionSchema";
 import {CollectionService} from "./services/CollectionService";
+import {FactSchema} from "./schemas/FactSchema";
 import {LimitOffsetDto} from "../../common/dto/LimitOffsetDto";
 import {ListFactsInputDto} from "./dto/ListFactsInputDto";
+import {ObjectId} from "mongodb";
 import {publicProcedure} from "@repo/facts-db";
 import {router} from "../../lib/trpc";
 import {z} from "zod";
@@ -11,13 +14,16 @@ export const createCollectionsRouter = () =>
   router({
     list: publicProcedure
       .input(LimitOffsetDto)
+      .output(z.array(CollectionSchema))
       .query(({input}) => collections.list(input)),
 
     get: publicProcedure
-      .input(z.string())
-      .query(({input}) => collections.get(input)),
+      .input(z.string().refine(ObjectId.isValid, {message: "Invalid ObjectId"}))
+      .output(CollectionSchema.nullish())
+      .query(({input}) => collections.get(input.toString())),
 
     listFacts: publicProcedure
       .input(ListFactsInputDto)
+      .output(z.array(FactSchema))
       .query(({input}) => collections.listFacts(input)),
   });
