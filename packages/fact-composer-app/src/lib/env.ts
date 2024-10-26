9e9a00add81a1396fab3fcad8e7e66b561e29c39
dsfx3d <dsfx3d@gmail.com>
2024-10-26 23:37:18 +0530

    refactor(env): comment out unused code related to environment variables

--- packages/fact-composer-app/src/lib/env.ts
+++ packages/fact-composer-app/src/lib/env.ts
@@ -1,7 +1,7 @@
-import {z} from "zod";
+// import {z} from "zod";
 
-const EnvSchema = z.object({
-  VITE_MARKETPLACE_API_URL: z.string(),
-});
+// const EnvSchema = z.object({
+//   VITE_MARKETPLACE_API_URL: z.string(),
+// });
 
-export const env = EnvSchema.parse(import.meta.env);
+// export const env = EnvSchema.parse(import.meta.env);
