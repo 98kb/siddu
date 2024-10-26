90a7c27d34412ca7b2cc89ace4c70961eb573fc9
dsfx3d <dsfx3d@gmail.com>
2024-10-26 23:25:22 +0530

    Remove unused code related to Chrome authentication

--- packages/chrome-auth-service/src/createAppRouter.ts
+++
@@ -1,7 +0,0 @@
-import {publicProcedure, router} from "./lib/trpc";
-import {IAuthService} from "./services/IAuthService";
-
-export const createAppRouter = (auth: IAuthService) =>
-  router({
-    getAccessToken: publicProcedure.query(() => auth.getAccessToken()),
-  });
--- packages/chrome-auth-service/src/createChromeAuthRuntimeServer.ts
+++
@@ -1,9 +0,0 @@
-import {createAppRouter} from "./createAppRouter";
-import {createChromeHandler} from "trpc-chrome/adapter";
-import {ChromeAuth} from "./services/ChromeAuth";
-
-export const createChromeAuthRuntimeServer = () =>
-  createChromeHandler({
-    router: createAppRouter(new ChromeAuth()),
-    onError: console.error,
-  });
