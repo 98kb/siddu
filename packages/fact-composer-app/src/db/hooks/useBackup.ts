6593578958db54ab03bcd01c88c28a471ec80e1e
dsfx3d <dsfx3d@gmail.com>
2024-10-30 11:08:24 +0530

    feat(fact-composer): add useBackup hook

--- packages/fact-composer-app/src/db/hooks/useBackup.ts
+++ packages/fact-composer-app/src/db/hooks/useBackup.ts
@@ -1,6 +1,38 @@
-import {useContext} from "react";
+import {useAtom} from "jotai";
+import {useCallback, useContext} from "react";
 import {BackupContext} from "~/context/BackupContext";
+import {isBackingUpAtom} from "../stores/isBackingUpAtom";
+import {isRestoringAtom} from "../stores/isRestoringAtom";
 
 export function useBackup() {
-  return useContext(BackupContext);
+  const client = useContext(BackupContext);
+  const [isBackingUp, setIsBackingUp] = useAtom(isBackingUpAtom);
+  const [isRestoring, setIsRestoring] = useAtom(isRestoringAtom);
+  const isBusy = isBackingUp || isRestoring;
+
+  const backup = useCallback(async () => {
+    if (isBusy) {
+      return;
+    }
+    setIsBackingUp(true);
+    try {
+      await client?.backup.mutate();
+    } finally {
+      setIsBackingUp(false);
+    }
+  }, [client, isBusy, setIsBackingUp]);
+
+  const restore = useCallback(async () => {
+    if (isBusy) {
+      return;
+    }
+    setIsRestoring(true);
+    try {
+      await client?.restore.mutate();
+    } finally {
+      setIsRestoring(false);
+    }
+  }, [client, isBusy, setIsRestoring]);
+
+  return {isBusy, backup, restore};
 }
---
+++ packages/fact-composer-app/src/db/stores/isRestoringAtom.ts
@@ -0,0 +1,3 @@
+import {atom} from "jotai";
+
+export const isRestoringAtom = atom<boolean>(false);
