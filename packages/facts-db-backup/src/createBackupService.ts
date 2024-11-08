e016fbcb02ebaec0ef5ff72630af511cf1da67a2
dsfx3d <dsfx3d@gmail.com>
2024-11-08 21:19:38 +0530

    feat(export-import): import latest records only

--- packages/facts-db-backup/src/createBackupService.ts
+++ packages/facts-db-backup/src/createBackupService.ts
@@ -1,14 +1,14 @@
 import {BackupService} from "./services/BackupService";
-import {type Dexie} from "dexie";
 import {DriveClient} from "./services/DriveClient";
 import {DriveUploader} from "./services/DriveUploader";
 import {ExportImportService} from "./services/ExportImportService";
+import {FactsDB} from "@repo/facts-db";
 import {GoogleDriveService} from "./services/GoogleDriveService";
 import {type IAuthService} from "@repo/chrome-auth-service";
 import {NaamkaranService} from "./services/NaamkaranService";
 
 export function createBackupService(
-  db: Dexie,
+  db: FactsDB,
   auth: IAuthService,
 ): BackupService {
   const client = new DriveClient(auth);
--- packages/facts-db-backup/src/services/ExportImportService.ts
+++ packages/facts-db-backup/src/services/ExportImportService.ts
@@ -1,19 +1,37 @@
-import {type Dexie} from "dexie";
+import {Fact, FactsDB, Label} from "@repo/facts-db";
 import {exportDB, importInto} from "dexie-export-import";
 
-/**
- * TODO: Encryption and compression
- */
+// TODO: Encryption and compression
 export class ExportImportService {
-  constructor(private db: Dexie) {}
+  constructor(private db: FactsDB) {}
 
   async export(): Promise<Blob> {
     return exportDB(this.db);
   }
 
+  // TODO: cleanup import approach
   async import(data: Blob) {
+    const dbData = await this.toDbData();
+
     return importInto(this.db, data, {
       overwriteValues: true,
+      filter: (tableName: string, value: {id: number; updatedAt: number}) => {
+        if (dbData[tableName]?.[value.id]) {
+          const table = dbData[tableName];
+          return !(table[value.id].updatedAt! > value.updatedAt);
+        }
+        return true;
+      },
     });
+  }
+
+  private async toDbData(): Promise<
+    Record<string, Record<number, {updatedAt?: number}>>
+  > {
+    const facts: Record<number, Fact> = {};
+    const labels: Record<number, Label> = {};
+    await this.db.facts.each($fact => (facts[$fact.id] = $fact));
+    await this.db.labels.each($label => (labels[$label.id] = $label));
+    return {facts, labels};
   }
 }
