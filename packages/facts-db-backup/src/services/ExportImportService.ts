0789a8ed9f48193ef65a887479d556b6aa45c695
dsfx3d <dsfx3d@gmail.com>
2024-11-10 00:07:33 +0530

    feat(facts-db-backup): enhance import with soft delete handling

--- packages/facts-db-backup/src/services/ExportImportService.ts
+++ packages/facts-db-backup/src/services/ExportImportService.ts
@@ -1,5 +1,8 @@
-import {Fact, FactsDB, Label} from "@repo/facts-db";
+import {BaseSchema, Fact, FactsDB, Label} from "@repo/facts-db";
 import {exportDB, importInto} from "dexie-export-import";
+import {z} from "zod";
+
+type BaseSchema = z.infer<typeof BaseSchema>;
 
 // TODO: Encryption and compression
 export class ExportImportService {
@@ -15,18 +18,25 @@ export class ExportImportService {
 
     return importInto(this.db, data, {
       overwriteValues: true,
-      filter: (tableName: string, value: {id: number; updatedAt: number}) => {
+      filter: (tableName: string, value: BaseSchema) => {
         if (dbData[tableName]?.[value.id]) {
           const table = dbData[tableName];
-          return !(table[value.id].updatedAt! > value.updatedAt);
+          return !(table[value.id].updatedAt! > value.updatedAt!);
         }
         return true;
+      },
+      transform: (tableName: string, value: BaseSchema, key: any) => {
+        // if a record is soft deleted either locally or remotely then soft delete it locally
+        // because we don't want to resurrect a deleted record
+        const isDeleted =
+          dbData[tableName]?.[value.id]?.isDeleted || value.isDeleted;
+        return {value: {...value, isDeleted}, key};
       },
     });
   }
 
   private async toDbData(): Promise<
-    Record<string, Record<number, {updatedAt?: number}>>
+    Record<string, Record<number, BaseSchema>>
   > {
     const facts: Record<number, Fact> = {};
     const labels: Record<number, Label> = {};
