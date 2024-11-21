f4459e3e95fb402733e30f0a7be0999bdfd1428d
dsfx3d <dsfx3d@gmail.com>
2024-11-21 10:39:14 +0530

    feat(collection-service-helpers-dexie): update database schema to use UUIDs for primary keys

--- packages/collection-service-helpers-dexie/src/db/createCollectionDB.ts
+++ packages/collection-service-helpers-dexie/src/db/createCollectionDB.ts
@@ -1,12 +1,13 @@
 import {CollectionDB} from "./CollectionDB";
+import {v4} from "uuid";
 import Dexie from "dexie";
 import type {BaseSchema} from "@repo/collection-service-defs";
 
 export function createCollectionDB(name: string): CollectionDB {
   const db = new Dexie(name) as CollectionDB;
   db.version(1).stores({
-    facts: "++id, title, content, updatedAt",
-    labels: "++id, name, updatedAt",
+    facts: "++_id, title, content, updatedAt",
+    labels: "++_id, name, updatedAt",
   });
   addUpdatedAtHook(db);
   return db;
@@ -16,6 +17,7 @@ function addUpdatedAtHook(db: CollectionDB) {
   for (const table of [db.facts, db.labels]) {
     table.hook("creating", function (_, obj) {
       obj.updatedAt = Date.now();
+      obj._id = v4();
     });
     table.hook("updating", function (modifications) {
       (modifications as BaseSchema).updatedAt = Date.now();
