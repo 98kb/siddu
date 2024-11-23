683cebd9faa93613e76e76aaa36beebe5eb4ea95
dsfx3d <dsfx3d@gmail.com>
2024-11-23 22:05:38 +0530

    feat(fact-composer-app): add ArchiveRestoreFact component for archiving and restoring facts

---
+++ packages/fact-composer-app/src/pages/collection/features/ArchiveRestoreFact.tsx
@@ -0,0 +1,39 @@
+import {ArchiveRestoreButton} from "~/components/ArchiveRestoreButton";
+import {ComponentProps, useCallback} from "react";
+import {FactSchema} from "@repo/collection-service-defs";
+import {useFactActions} from "../hooks/useFactActions";
+import {Reader} from "fp-ts/lib/Reader";
+
+type TProps = {
+  fact: FactSchema;
+  size?: ComponentProps<typeof ArchiveRestoreButton>["size"];
+  onChange?: Reader<FactSchema | undefined, void>;
+};
+
+export function ArchiveRestoreFact({fact, size, onChange}: TProps) {
+  const {archiveFact, restoreFact} = useFactActions();
+
+  const archive = useCallback(
+    async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      event.stopPropagation();
+      onChange?.(await archiveFact(fact));
+    },
+    [archiveFact, fact, onChange],
+  );
+
+  const restore = useCallback(
+    async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      event.stopPropagation();
+      onChange?.(await restoreFact(fact));
+    },
+    [restoreFact, fact, onChange],
+  );
+  return (
+    <ArchiveRestoreButton
+      size={size}
+      onArchive={archive}
+      onRestore={restore}
+      restore={Boolean(fact.isDeleted)}
+    />
+  );
+}
