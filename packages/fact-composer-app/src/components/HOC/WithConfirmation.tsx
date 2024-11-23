89159c282e3df411513cdf0fc4857370a7bd439f
dsfx3d <dsfx3d@gmail.com>
2024-11-23 23:22:15 +0530

    feat(fact-composer-app): refactor WithConfirmation HOC to improve onClick handling and support button props

--- packages/fact-composer-app/src/components/HOC/WithConfirmation.tsx
+++ packages/fact-composer-app/src/components/HOC/WithConfirmation.tsx
@@ -1,36 +1,44 @@
-import {IO} from "fp-ts/lib/IO";
-import {Task} from "fp-ts/lib/Task";
-import {useCallback, useState} from "react";
+import React, {ComponentProps, useCallback, useState} from "react";
 import {ConfirmButtons} from "~/components/ConfirmButtons";
+import {Button} from "../ui/button";
 
 type TProps<MoreProps> = Omit<MoreProps, "onClick"> & {
-  onConfirm?: Task<void> | IO<void>;
-  onCancel?: Task<void> | IO<void>;
-  For: React.FC<MoreProps & {onClick: IO<void>}>;
+  label?: React.ReactNode;
+  onConfirm?: ComponentProps<"button">["onClick"];
+  onCancel?: ComponentProps<"button">["onClick"];
+  For: React.FC<ComponentProps<typeof Button>>;
 };
 
-export function WithConfirmation<MoreProps extends {onClick: IO<void>}>({
-  For,
-  onConfirm,
-  onCancel,
-  ...moreProps
-}: TProps<MoreProps>) {
+export function WithConfirmation<
+  MoreProps extends ComponentProps<typeof Button>,
+>({label, For, onConfirm, onCancel, size, ...moreProps}: TProps<MoreProps>) {
   const innerProps = moreProps as unknown as MoreProps;
   const [isConfirming, setIsConfirming] = useState<boolean>(false);
-  const confirm = useCallback(async () => {
-    await onConfirm?.();
-    setIsConfirming(false);
-  }, [onConfirm]);
-  const cancel = useCallback(async () => {
-    await onCancel?.();
-    setIsConfirming(false);
-  }, [onCancel]);
+  const confirm = useCallback(
+    async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      onConfirm?.(event);
+      setIsConfirming(false);
+    },
+    [onConfirm],
+  );
+  const cancel = useCallback(
+    async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      onCancel?.(event);
+      setIsConfirming(false);
+    },
+    [onCancel],
+  );
+  const onClick = useCallback<React.MouseEventHandler>(event => {
+    event.stopPropagation();
+    setIsConfirming(true);
+  }, []);
 
   return isConfirming ? (
-    <div className="flex gap-3">
-      <ConfirmButtons size="icon" onConfirm={confirm} onCancel={cancel} />
+    <div className="flex gap-3 items-center">
+      {label}
+      <ConfirmButtons size={size} onConfirm={confirm} onCancel={cancel} />
     </div>
   ) : (
-    <For {...innerProps} onClick={() => setIsConfirming(true)} />
+    <For {...innerProps} size={size} onClick={onClick} />
   );
 }
