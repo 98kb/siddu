8db7529c179854faca1bc6e27baf75324273835e
dsfx3d <dsfx3d@gmail.com>
2024-11-24 15:48:52 +0530

    refactor(FactCardActions.stories): remove unused onClickPropagation arg from story

--- packages/fact-composer-app/src/pages/collection/features/FactCardActions.stories.tsx
+++ packages/fact-composer-app/src/pages/collection/features/FactCardActions.stories.tsx
@@ -1,13 +1,9 @@
 import type {Meta, StoryObj} from "@storybook/react";
 import {FactCardActions} from "./FactCardActions";
-import {fn} from "@storybook/test";
 
 const meta: Meta<typeof FactCardActions> = {
   title: "collection/features/FactCardActions",
   component: FactCardActions,
-  args: {
-    onClickPropagation: fn(),
-  },
 };
 
 export default meta;
