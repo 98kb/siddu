8ec259c8796ac36b0a67065a76c971d5c5375021
dsfx3d <dsfx3d@gmail.com>
2024-11-24 22:50:49 +0530

    feat(ListLabels): implement custom hook for paginated label listing and navigation

--- packages/fact-composer-app/src/pages/labels/features/ListLabels.tsx
+++ packages/fact-composer-app/src/pages/labels/features/ListLabels.tsx
@@ -1,10 +1,43 @@
 import {LabelsTable} from "../organism/LabelsTable";
-import {useLabelsQuery} from "../hooks/useLabelsQuery";
 import {useLabelActions} from "../hooks/useLabelActions";
+import {useCallback, useEffect, useState} from "react";
+import {LabelSchema} from "@repo/collection-service-defs";
+import {useLabelsApi} from "../hooks/useLabelsApi";
 
 export function ListLabels() {
   const {softDeleteLabel} = useLabelActions();
-  const {labels} = useLabelsQuery();
+  const {labels} = useListLabels();
 
   return <LabelsTable labels={labels} onDelete={softDeleteLabel} />;
+}
+
+// eslint-disable-next-line max-statements
+function useListLabels() {
+  const {toPaginatedLabels} = useLabelsApi();
+  const limit = 2;
+  const [total, setTotal] = useState(0);
+  const [offset, setOffset] = useState(0);
+  const [labels, setLabels] = useState<LabelSchema[]>([]);
+  const nextPage = useCallback(() => {
+    setOffset($offset => $offset + 1);
+  }, []);
+  const prevPage = useCallback(() => {
+    setOffset($offset => $offset - 1);
+  }, []);
+  useEffect(() => {
+    toPaginatedLabels({
+      pagination: {limit, offset},
+      isDeleted: false,
+      // eslint-disable-next-line complexity
+    }).then(result => {
+      setLabels(result?.list ?? []);
+      setTotal(result?.total ?? 0);
+    });
+  }, [offset, toPaginatedLabels]);
+  return {
+    total,
+    labels,
+    nextPage,
+    prevPage,
+  };
 }
