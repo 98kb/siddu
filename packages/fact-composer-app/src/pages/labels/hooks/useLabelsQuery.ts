53fb0cbb49c4af6507134c1b73251d84a54842af
dsfx3d <dsfx3d@gmail.com>
2024-11-25 07:17:49 +0530

    feat(useLabelsQuery): rename toLabels function to listLabels for clarity

--- packages/fact-composer-app/src/pages/labels/hooks/useLabelsQuery.ts
+++ packages/fact-composer-app/src/pages/labels/hooks/useLabelsQuery.ts
@@ -4,15 +4,15 @@ import {useLabelsApi} from "./useLabelsApi";
 import {queryLabelsAtom} from "../store/queryLabelsAtom";
 
 export function useLabelsQuery() {
-  const {toLabels} = useLabelsApi();
+  const {listLabels} = useLabelsApi();
   const [labels, setLabels] = useAtom(queryLabelsAtom);
   const fetchLabels = useCallback(
     async () =>
-      toLabels({
+      listLabels({
         pagination: {limit: 99, offset: 0},
         isDeleted: false,
       }).then($labels => $labels && setLabels($labels)),
-    [toLabels, setLabels],
+    [listLabels, setLabels],
   );
   useEffect(() => {
     fetchLabels();
