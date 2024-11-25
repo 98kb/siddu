7c19e1216a030f89e084ae5f7a6ba5ba232094d2
dsfx3d <dsfx3d@gmail.com>
2024-11-25 07:18:37 +0530

    refactor(useLabelActions): simplify addLabel and softDeleteLabel functions

--- packages/fact-composer-app/src/pages/labels/hooks/useLabelActions.ts
+++ packages/fact-composer-app/src/pages/labels/hooks/useLabelActions.ts
@@ -1,17 +1,15 @@
 import {InsertLabelSchema, LabelSchema} from "@repo/collection-service-defs";
-import {Reader} from "fp-ts/lib/Reader";
 import {useCallback} from "react";
 import {useCollection} from "~/pages/collection/hooks/useCollection";
 
 export function useLabelActions() {
   const collection = useCollection();
-  const addLabel = useCallback<
-    Reader<InsertLabelSchema, Promise<LabelSchema> | undefined>
-  >(label => collection?.labels.create.mutate(label), [collection]);
-  const softDeleteLabel = useCallback<
-    Reader<LabelSchema["_id"], Promise<LabelSchema | undefined> | undefined>
-  >(
-    labelId => collection?.labels.softDelete.mutate({_id: labelId}),
+  const addLabel = useCallback(
+    (label: InsertLabelSchema) => collection?.labels.create.mutate(label),
+    [collection],
+  );
+  const softDeleteLabel = useCallback(
+    (_id: LabelSchema["_id"]) => collection?.labels.softDelete.mutate({_id}),
     [collection],
   );
   return {addLabel, softDeleteLabel};
