118a2550a47487de882a4f0c5bed99d34512b9da
dsfx3d <dsfx3d@gmail.com>
2024-11-26 20:08:14 +0530

    feat(CollectionNav): enhance navigation with animated archive tab and refactor tab rendering

--- packages/fact-composer-app/src/pages/collection/features/CollectionNav.tsx
+++ packages/fact-composer-app/src/pages/collection/features/CollectionNav.tsx
@@ -1,17 +1,16 @@
 import {BookDownIcon, BookOpenTextIcon} from "lucide-react";
 import {LabelIcon} from "../components/LabelIcon";
 import {NavTab} from "~/lib/NavTab";
-import {useEffect, useMemo} from "react";
+import {ComponentProps, useEffect, useMemo, useState} from "react";
 import {CollectionNavTab} from "../components/CollectionNavTab";
 import {useFactFilters} from "../hooks/useFactFilters";
-import {useLocation, useNavigate} from "react-router-dom";
 import {useLabelsQuery} from "~/pages/labels/hooks/useLabelsQuery";
 import {AddFactButton} from "./AddFactButton";
+import {bind} from "@react-rxjs/core";
+import {factArchived$} from "../signals/factArchived";
 
 export function CollectionNav() {
   const tabs = useCollectionNav();
-  const navigate = useNavigate();
-  const location = useLocation();
   const {fetchLabels} = useLabelsQuery();
 
   useEffect(() => {
@@ -27,19 +26,28 @@ export function CollectionNav() {
           className="p-4 pr-6 rounded-full"
         />
       </div>
-      {tabs.map(({name, Icon, route}) => (
-        // TODO: convert this component to a feature
-        <CollectionNavTab
-          key={name}
-          name={name}
-          isActive={`${location.pathname}${location.search}` === route}
-          onClick={() => navigate(route!)}
-        >
-          <Icon key={name} className="w-4 h-4" />
-        </CollectionNavTab>
+      {tabs.slice(0, -1).map(tab => (
+        <CollectionNavTab key={tab.name} tab={tab} />
       ))}
+      <ArchiveNavTab tab={tabs.at(-1)!} />
     </aside>
   );
+}
+
+const [useArchivedFact] = bind(factArchived$, undefined);
+function ArchiveNavTab(props: ComponentProps<typeof CollectionNavTab>) {
+  const archivedFact = useArchivedFact();
+  const [className, setClassName] = useState("");
+  useEffect(() => {
+    if (archivedFact) {
+      setClassName("animate-shake");
+      setTimeout(() => setClassName(""), 800);
+      return () => {
+        setClassName("");
+      };
+    }
+  }, [archivedFact]);
+  return <CollectionNavTab {...props} className={className} />;
 }
 
 function useCollectionNav(): NavTab[] {
