f54cff6145b98415d05b125e1d3cb8f9948963a9
dsfx3d <dsfx3d@gmail.com>
2024-11-27 02:15:09 +0530

    refactor(ArchiveRestoreFact): update event handling to use onClick prop and streamline archiving logic

--- packages/fact-composer-app/src/pages/collection/features/ArchiveRestoreFact.tsx
+++ packages/fact-composer-app/src/pages/collection/features/ArchiveRestoreFact.tsx
@@ -7,33 +7,27 @@ import {Reader} from "fp-ts/lib/Reader";
 type TProps = {
   fact: FactSchema;
   size?: ComponentProps<typeof ArchiveRestoreButton>["size"];
-  onChange?: Reader<FactSchema | undefined, void>;
+  onClick?: Reader<FactSchema, void>;
 };
 
-export function ArchiveRestoreFact({fact, size, onChange}: TProps) {
-  const {archiveFact, restoreFact} = useArchiveFact();
-
-  const archive = useCallback(
-    async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+export function ArchiveRestoreFact({fact, size, onClick}: TProps) {
+  const canRestore = Boolean(fact.isDeleted);
+  const {toggleArchive} = useArchiveFact();
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
       event.stopPropagation();
-      onChange?.(await archiveFact(fact));
+      onClick?.(fact);
+      // TODO: resolve coupling
+      // setting a timeout to allow the animation to complete
+      setTimeout(() => toggleArchive(fact), 150);
     },
-    [archiveFact, fact, onChange],
-  );
-
-  const restore = useCallback(
-    async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-      event.stopPropagation();
-      onChange?.(await restoreFact(fact));
-    },
-    [restoreFact, fact, onChange],
+    [onClick, toggleArchive, fact],
   );
   return (
     <ArchiveRestoreButton
       size={size}
-      onArchive={archive}
-      onRestore={restore}
-      restore={Boolean(fact.isDeleted)}
+      onClick={handleClick}
+      restore={canRestore}
     />
   );
 }
