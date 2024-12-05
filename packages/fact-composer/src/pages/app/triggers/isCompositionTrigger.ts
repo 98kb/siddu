a35c495352a240f797360de9fc9beefc1848ae14
dsfx3d <dsfx3d@gmail.com>
2024-12-06 01:31:30 +0530

    refactor(fact-composer): remove unused composition trigger hooks and logic

--- packages/fact-composer/src/pages/app/triggers/isCompositionTrigger.ts
+++
@@ -1,9 +0,0 @@
-import {Reader} from "fp-ts/lib/Reader";
-
-export const isCompositionTrigger: Reader<KeyboardEvent, boolean> = event =>
-  event.key === "." && event.ctrlKey && isInputTarget(event);
-
-const isInputTarget: Reader<KeyboardEvent, boolean> = event =>
-  event.target instanceof HTMLInputElement ||
-  event.target instanceof HTMLTextAreaElement ||
-  (event.target as HTMLElement).contentEditable === "true";
